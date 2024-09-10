922d70ff7b85ad5cf921fc148418e1e8530c0e62
jxu <7989982+jxu@users.noreply.github.com>
2024-09-09 23:49:33 -0400

    Clean up and combine print_dec and print_dec2

--- print_dec.c
+++ print_dec.c
@@ -1,31 +1,54 @@
-// print non-negative integer in decimal
+#include <stdint.h>
 #include <stdio.h>
+#include <unistd.h>
 
-typedef unsigned long long ull;
+// print non-negative 64-bit integer in decimal
+void print_dec(uint64_t x) 
+{
+    char stack[20]; // simulated stack
+    char* sp = stack; // not actual rsp
+    char* bp = sp;
 
-
-void print_dec(ull x)
-{    
-    ull stack[20];
-    ull sp = 0; // not actual x86 sp
-    ull bp = sp;
-    ull d;
-
-    do // run at least once to print 0
+    // push digits LSD to MSD
+    do  // run at least once to print 0
     {
-        d = x % 10; 
-        stack[sp++] = d + '0'; // push
+        char d = (x % 10) + '0';
+        *(sp++) = d;  // ugly terse C syntax for push
         x /= 10;
     } while (x);
 
+    // pop and write digits MSD to LSD
     do 
     {
-        d = stack[--sp]; // pop
+        char d; // for clarity
+        d = *(--sp); // pop
         putc(d, stdout);
     } while (sp != bp);
 }
 
-int main() 
+// print non-negative 64-bit integer in decimal
+// with one call on a stack reading bottom-up
+void print_dec2(uint64_t x) 
 {
-    print_dec(1234);
+    char stack[20];
+    char *sp = stack + 20; // end of array
+    char digits = 0;
+
+    do  // run at least once to print 0
+    {
+        *(--sp) = (x % 10) + '0';  // push from end
+        x /= 10;
+        ++digits;
+    } while (x);
+
+    // one write call in C. credit chux
+    // can't push single bytes to actual x86 stack though
+    write(1, sp, digits);
+}
+
+
+int main() 
+{ 
+    //print_dec(1234); 
+    print_dec2(1234);
 }
